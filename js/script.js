689fdb064b66de2de6a30ba800df8d12feac660f
ibrahim <olaniyiibrahim2006@gmail.com>
2024-07-27 15:52:20 -0700

    commit message

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const menu = document.getElementById('menu')
 btn.addEventListener('click', navToggle)
 
 function navToggle() {
-  btn.classList.toggle('open')
+  btn.classList.toggle('open') 
   menu.classList.toggle('flex')
   menu.classList.toggle('hidden')
 }
